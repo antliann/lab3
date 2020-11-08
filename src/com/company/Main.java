30efff51cac0e19dc2f06e29e9fc541373c106f8
Anton <antonliann@gmail.com>
2020-11-08 22:32:44 +0200

    Buf fixes

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,9 +3,7 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
 
-
 public class Main {
-
     public static void main(String[] args) {
         try {
             File myObj = new File("program.c");
